b94b6e5f70bb79ef94bd40bc56d02a39db6f3f4d
Yashaswini <yashu27aug@gmail.com>
2024-10-18 21:31:24 +0530

    updated

--- generate_fc.py
+++ generate_fc.py
@@ -27,6 +27,18 @@ for subject in subjects:
     ts_concat = load_timeseries(subject, name=args.task, dir=task_dir)
     fmri_data.append(ts_concat)
 
-fmri_data = np.array(fmri_data)
-print(f"Loaded fMRI data for task '{args.task}'")
-print(f"Shape = {fmri_data.shape}")
+fmri_data_numpy = np.array(fmri_data)
+
+print(f"Loaded fMRI data for '{args.task}'")
+print(f"Shape = {fmri_data_numpy.shape}")
+
+fc = np.zeros((N_SUBJECTS, N_PARCELS, N_PARCELS))
+for sub, ts in enumerate(fmri_data):
+  fc[sub] = np.corrcoef(ts)
+
+print(f"Generated Functional Connectomes")
+
+# Save the functional connectivity array
+fc_filename = os.path.join('./FC_DATA', f'fc_{args.task}.npy')  # Define the filename
+np.save(fc_filename, fc)  # Save the array
+print(f"Saved functional connectivity matrix to '{fc_filename}'")
